9143f8e97d29c64c27976d3147abed4435aa802b
Jon Dowland <jmtd@debian.org>
2010-09-04 18:07:27 +0100

    rework pureIncrement... to use tuples

--- pureincrementReadDirectory.hs
+++ pureincrementReadDirectory.hs
@@ -9,31 +9,40 @@ For now, assume there is only one directory involved - reading /
 -}
 
 import Data.List
+import Control.Arrow -- first,second
 
 -- test data
-currentDirectory = [ "b", "rdiff-backup-data" ]
-increDirectory   = [ "a.2010-09-04T12:49:27+01:00.snapshot.gz",
-	"a.2010-10-04T12:49:27+01:00.snapshot.gz",
-	"b.2010-09-04T12:49:27+01:00.missing" ]
+currentDirectory = [ ("b", File), ("rdiff-backup-data", Dir) ]
+increDirectory   = [ ("a.2010-09-04T12:49:27+01:00.snapshot.gz", File),
+	("a.2010-10-04T12:49:27+01:00.snapshot.gz", File),
+	("c.2010-10-04T12:49:27+01:00.dir", File),
+	("b.2010-09-04T12:49:27+01:00.missing", File) ]
 increment = "2010-09-04T12:49:27+01:00"
+increment2 = "2010-10-04T12:49:27+01:00"
 
-currentReadDirectory :: [FilePath]
-currentReadDirectory = filter (/= "rdiff-backup-data") currentDirectory
+-- convenience types
+data Ftype = File | Dir deriving (Show) -- simplification
+type Fpair = (FilePath, Ftype)
 
-incrementReadDirectory :: String -> [FilePath]
-incrementReadDirectory incr = nub
-	(incfiles ++ dirfiles ++ difffiles ++ currentReadDirectory) \\ missfiles where
+currentReadDirectory :: [Fpair]
+currentReadDirectory = filter ((/= "rdiff-backup-data") . fst) currentDirectory
+
+incrementReadDirectory :: String -> [Fpair]
+incrementReadDirectory incr = nubBy pairCmp $
+	(incfiles ++ dirfiles ++ difffiles ++ currentReadDirectory) \\\ missfiles where
 	incfiles  = fetch ".snapshot.gz"
 	missfiles = fetch ".missing"
 	difffiles = fetch ".diff.gz"
-	dirfiles  = fetch ".dir"
+	dirfiles  = map (second (\_->Dir)) (fetch ".dir")
 	fetch s = hasSuffix ('.':incr) $ hasSuffix s increDirectory
+	pairCmp (a,_) (b,_) = a == b
+	(\\\) = deleteFirstsBy pairCmp -- specialised '\\'
 
 -- returns sublist of strings which have the provided suffix,
 -- with the suffix removed
-hasSuffix :: String -> [FilePath] -> [FilePath]
+hasSuffix :: String -> [Fpair] -> [Fpair]
 hasSuffix _ [] = []
 hasSuffix suffix fps = 
-	map (trimSuffix suffix) $ filter (isSuffixOf suffix) fps
+	map (first (trimSuffix suffix)) $ filter (isSuffixOf suffix . fst) fps
 	where
 		trimSuffix s = reverse . (drop (length s)) . reverse
