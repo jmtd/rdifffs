b7df14975b87a8aed2b58462dab20d96bb7bbfcd
Jon Dowland <jmtd@debian.org>
2010-09-06 22:33:18 +0100

    introduce hfuse types to pure code
    In order to get the pure code closer and closer to the types needed
    by the real code, introduce the FileStat and related types from Hfuse
    into the pure code.
    
    In order to not corrupt the pure code too much, placeholder FileStat
    constructions for files and directories are defined.
    
    If currentDirectory and IncreDirectory defined accurate FileStat
    structures, then the output of incrementReadDirectory would include
    the correct metadata for the final files. Thus this should actually
    work properly once plumbed into the Fuse mechanics :-)

--- RdiffFS.hs
+++ RdiffFS.hs
@@ -16,20 +16,40 @@ For now, assume there is only one directory involved - reading /
 -}
 
 import Data.List
+import System.Fuse -- EntryType, FileStat
+import System.Posix.Files -- stdFileMode
 import Control.Arrow -- first,second
 
+-- default FileStat object, for when we don't care
+buildStat entrytype = FileStat { statEntryType = entrytype
+ , statFileMode = stdFileMode, statLinkCount = 2,       statFileOwner = 0
+ , statFileGroup = 0,          statSpecialDeviceID = 0, statFileSize = 1024
+ , statBlocks = 1,             statAccessTime = 0
+ , statModificationTime = 0, statStatusChangeTime = 0
+ }
+defaultDir  = buildStat Directory
+defaultFile = buildStat RegularFile
+
+-- convenience types
+type Fpair = (FilePath, FileStat)
+
+-- hacks thing to make interactive use of above easier
+s :: Fpair -> (FilePath, String)
+s = second s' where
+    s' y = case (statEntryType y) of
+              RegularFile -> "File"
+              Directory   -> "Dir"
+              _           -> "?"
+
 -- test data
-currentDirectory = [ ("b", File), ("rdiff-backup-data", Dir) ]
-increDirectory   = [ ("a.2010-09-04T12:49:27+01:00.snapshot.gz", File),
-	("a.2010-10-04T12:49:27+01:00.snapshot.gz", File),
-	("c.2010-10-04T12:49:27+01:00.dir", File),
-	("b.2010-09-04T12:49:27+01:00.missing", File) ]
+currentDirectory = [ ("b", defaultFile), ("rdiff-backup-data", defaultDir) ]
+increDirectory   = [ ("a.2010-09-04T12:49:27+01:00.snapshot.gz", defaultFile),
+	("a.2010-10-04T12:49:27+01:00.snapshot.gz", defaultFile),
+	("c.2010-10-04T12:49:27+01:00.dir", defaultFile),
+	("b.2010-09-04T12:49:27+01:00.missing", defaultFile) ]
 increment = "2010-09-04T12:49:27+01:00"
 increment2 = "2010-10-04T12:49:27+01:00"
 
--- convenience types
-data Ftype = File | Dir deriving (Show) -- simplification
-type Fpair = (FilePath, Ftype)
 
 currentReadDirectory :: [Fpair]
 currentReadDirectory = filter ((/= "rdiff-backup-data") . fst) currentDirectory
@@ -43,7 +63,7 @@ incrementReadDirectory incr = nub' $
 	incfiles  = fetch ".snapshot.gz"
 	missfiles = fetch ".missing"
 	difffiles = fetch ".diff.gz"
-	dirfiles  = map (second (\_->Dir)) (fetch ".dir")
+	dirfiles  = map (second (\_->defaultDir)) (fetch ".dir")
 	fetch s = getBySuffix ('.':incr) $ getBySuffix s increDirectory
 
 -- returns sublist of strings which have the provided suffix,
