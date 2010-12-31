4431ea340427ccb704b2754bd5a0597620b59915
Jon Dowland <jmtd@debian.org>
2010-12-31 14:36:40 +0000

    decompress patches

--- util/grepCopy.hs
+++ util/grepCopy.hs
@@ -12,19 +12,22 @@ import System.Environment -- getArgs
 import System.Posix.Files -- fileExist
 import Monad -- filterM
 import Rdiff
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Codec.Compression.GZip as GZip
 
 -- is the rdiffCommand a CopyCommand?
 isCopyCommand :: RdiffCommand -> Bool
 isCopyCommand (CopyCommand _ _) = True
 isCopyCommand _ = False
 
--- does the filepath correspond to an rdiff patch which
+-- does the filepath correspond to a gzipped rdiff patch containing
+-- a copy instruction?
 containsCopy :: FilePath -> IO Bool
 containsCopy fp = do
   e <- fileExist fp
   if e then do
-      c <- readFile fp
-      case parsePatch c of
+      c <- fmap GZip.decompress (L.readFile fp)
+      case parsePatch (L.unpack c) of
         Left _ -> return False
         Right ps -> return $ length (filter isCopyCommand ps) > 0
     else return False
